74c523275d50b26ef45aa10f0e1218a6f7b73cca
subimeDev <riquelmemedinaluismatias@gmail.com>
2025-12-19 17:37:53 -0300

    fix: actualizar segunda ubicaciÃ³n del menÃº con sellos

--- frontend-ubold/src/layouts/components/data.ts
+++ frontend-ubold/src/layouts/components/data.ts
@@ -267,8 +267,8 @@ export const horizontalMenuItems: MenuItemType[] = [
                 children: [
                   { key: 'all-obras', label: 'Todas las Obras', url: '/atributos/obras' },
                   { key: 'add-obra', label: 'Agregar Obra', url: '/atributos/obras/agregar' },
-                  { key: 'all-tipo-libro', label: 'Todos los Tipos de Libro', url: '/atributos/tipo-libro' },
-                  { key: 'add-tipo-libro', label: 'Agregar Tipo de Libro', url: '/atributos/tipo-libro/agregar' },
+                  { key: 'all-sellos', label: 'Todos los Sellos', url: '/atributos/sello' },
+                  { key: 'add-sello', label: 'Agregar Sello', url: '/atributos/sello/agregar' },
                 ],
               },
               { key: 'product-requests', label: 'Solicitudes de Productos', url: '/products/solicitudes', roles: ['super_admin', 'encargado_adquisiciones', 'supervisor'] },
