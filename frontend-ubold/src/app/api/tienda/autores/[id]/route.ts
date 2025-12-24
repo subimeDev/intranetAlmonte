11acf3310db47f532adc7aac4d59f6d20d974433
Zenn-Dev99 <martininfantezuniga@gmail.com>
2025-12-24 09:46:47 -0300

    Fix: Agregar import faltante de logActivity en autores/[id]/route.ts

--- frontend-ubold/src/app/api/tienda/autores/[id]/route.ts
+++ frontend-ubold/src/app/api/tienda/autores/[id]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import strapiClient from '@/lib/strapi/client'
+import { logActivity, createLogDescription } from '@/lib/logging'
 
 export const dynamic = 'force-dynamic'
 
